d6baa674b10bbcefa09fde89f5a5d6db3a63bd99
Tolibjon <tolibjohn@gmail.com>
2021-09-22 21:01:58 +0500

    Insert data into the 'owners' table

--- data.sql
+++ data.sql
@@ -13,3 +13,12 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
                             ('Angemon', '06/12/2005', 1, TRUE, 45),
                             ('Boarmon', '06/07/2005', 7, TRUE, 20.4),
                             ('Blossom', '10/13/1998', 3, TRUE, 17);
+
+-- Insert data into the owners table
+INSERT INTO owners (full_name, age) 
+                    VALUES  ('Sam Smith',  34),
+                            ('Jennifer Orwell', 19),
+                            ('Bob', 45),
+                            ('Melody Pond', 77),
+                            ('Dean Winchester', 14),
+                            ('Jodie Whittaker', 38);
