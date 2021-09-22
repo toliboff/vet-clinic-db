b0dfbcd8d091c6bf71123be214b3bf34f90818fa
Tolibjon <tolibjohn@gmail.com>
2021-09-22 15:42:10 +0500

    Add queries

--- data.sql
+++ data.sql
@@ -14,3 +14,52 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
                             ('Boarmon', '06/07/2005', 7, TRUE, 20.4),
                             ('Blossom', '10/13/1998', 3, TRUE, 17);
 
+
+-- ---------------------- TRANSACTION ---------------------------------
+-- Inside a transaction update the animals table by setting the species column to unspecified
+BEGIN;
+  UPDATE animals
+  SET species='unspecified';
+ROLLBACK;
+
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+BEGIN;
+  UPDATE animals
+  SET species='digimon'
+  WHERE name LIKE '%mon';
+
+  -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set
+  UPDATE animals
+  SET species='pokemon'
+  WHERE COALASCE(species, '')='';
+COMMIT;
+
+-- Delete all records in the animals table, then roll back the transaction
+BEGIN;
+  DELETE FROM animals;
+ROLLBACK;
+
+-- Inside a transaction:
+  -- Delete all animals born after Jan 1st, 2022.
+  -- Create a savepoint for the transaction.
+  -- Update all animals' weight to be their weight multiplied by -1.
+  -- Rollback to the savepoint
+  -- Update all animals' weights that are negative to be their weight multiplied by -1.
+  -- Commit transaction
+BEGIN;
+
+  DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+
+  SAVEPOINT RIP_PLANTMON;
+
+  UPDATE animals
+  SET weight_kg=weight_kg*-1;
+
+  ROLLBACK TO RIP_PLANTMON;
+
+  UPDATE animals
+  SET weight_kg=weight_kg*-1
+  WHERE weight_kg<0;
+
+COMMIT;
