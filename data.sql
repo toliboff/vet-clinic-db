9f047d66d9df44ff9a505fa7fabc54cf0c29ec22
Tolibjon <tolibjohn@gmail.com>
2021-09-22 14:52:05 +0500

    Commit the transaction

--- data.sql
+++ data.sql
@@ -19,3 +19,16 @@ BEGIN;
   UPDATE animals
   SET species='unspecified';
 ROLLBACK;
+
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+BEGIN;
+  UPDATE animals
+  SET species='digimon'
+  WHERE name LIKE '%mon';
+
+  -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set
+  UPDATE animals
+  SET species='pokemon'
+  WHERE COALASCE(species, '')='';
+COMMIT;
