c509b6c6f039b8d2985638e256085e8368a49310
Tolibjon <tolibjohn@gmail.com>
2021-09-27 14:42:07 +0500

    Create 'medeical_histories' table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,10 @@ CREATE TABLE patients (
   date_of_birth DATE
 );
 
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(50),
+  FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
